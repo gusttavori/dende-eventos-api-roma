46915cc544a89730698bac3e3e5c814bdedb827b
lasilva <lucas.almeida.las@gmail.com>
2026-02-02 00:41:09 -0300

    feat: modificando dependencia

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,14 +7,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    mavenLocal()
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("io.github.lasilva:dendeframework:1.0-SNAPSHOT")
-
+    implementation("io.github.lasilva:dendeframework:1.0.0")
 }
 
 tasks.test {
